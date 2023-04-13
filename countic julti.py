4f696938ea69f6df0a7b429bf2339d1bc7d0490d
BuildTools <unconfigured@null.spigotmc.org>
2023-04-13 17:10:11 +0700

    optimize code

--- countic julti.py
+++ countic julti.py
@@ -36,90 +36,57 @@ while True:
     
     for log_file_path in log_file_paths:
         nether_count = 0
+        stronghold_count = 0
+        edn_count = 0
+        bastion_count = 0
+        fortress_count = 0
         
         with open(log_file_path, 'r') as f:
             for line in f:
                 if "We Need to Go Deeper" in line:
                     nether_count += 1
-        
+                if "Eye Spy" in line:
+                    stronghold_count += 1    
+                if "The End?" in line:
+                    edn_count += 1
+                if "Those Were the Days" in line:
+                    bastion_count += 1
+                if "A Terrible Fortress" in line:
+                    fortress_count += 1
         nether_count //= 2
         total_nether_count += nether_count
+        stronghold_count //= 2
+        total_stronghold_count += stronghold_count
+        edn_count //= 2
+        total_edn_count += edn_count
+        bastion_count //= 2
+        total_bastion_count += bastion_count
+        fortress_count //= 2
+        total_fortress_count += fortress_count
         
         print("The phrase 'We Need to Go Deeper' appears", nether_count, "times in the file", log_file_path)
+        print("The phrase 'Eye Spy' appears", stronghold_count, "times in the file", log_file_path)
+        print("The phrase 'Eye Spy' appears", stronghold_count, "times in the file", log_file_path)
+        print("The phrase 'Those Were the Days' appears", bastion_count, "times in the file", log_file_path)
+        print("The phrase 'A Terrible Fortress' appears", fortress_count, "times in the file", log_file_path)
     
     print("The phrase 'We Need to Go Deeper' appears", total_nether_count, "times in all log files.")
+    print("The phrase 'Eye Spy' appears", total_stronghold_count, "times in all log files.")
+    print("The phrase 'The End?' appears", total_edn_count, "times in all log files.")
+    print("The phrase 'Those Were the Days' appears", total_bastion_count, "times in all log files.")
+    print("The phrase 'A Terrible Fortress' appears", total_fortress_count, "times in all log files.")
     
     with open("nether.txt", "w") as count_file:
         count_file.write(str(total_nether_count))
 
-    for log_file_path in log_file_paths:
-        stronghold_count = 0
-        
-        with open(log_file_path, 'r') as f:
-            for line in f:
-                if "Eye Spy" in line:
-                    stronghold_count += 1
-        
-        stronghold_count //= 2
-        total_stronghold_count += stronghold_count
-        
-        print("The phrase 'Eye Spy' appears", stronghold_count, "times in the file", log_file_path)
-    
-    print("The phrase 'Eye Spy' appears", total_stronghold_count, "times in all log files.")
-    
     with open("stronghold.txt", "w") as count_file:
         count_file.write(str(total_stronghold_count))
 
-    for log_file_path in log_file_paths:
-        edn_count = 0
-        
-        with open(log_file_path, 'r') as f:
-            for line in f:
-                if "The End?" in line:
-                    edn_count += 1
-        
-        edn_count //= 2
-        total_edn_count += edn_count
-        
-        print("The phrase 'The End?' appears", edn_count, "times in the file", log_file_path)
-    
-    print("The phrase 'The End?' appears", total_edn_count, "times in all log files.")
-    
     with open("edn.txt", "w") as count_file:
         count_file.write(str(total_edn_count))
-
-    for log_file_path in log_file_paths:
-        bastion_count = 0
-        
-        with open(log_file_path, 'r') as f:
-            for line in f:
-                if "Those Were the Days" in line:
-                    bastion_count += 1
-        
-        bastion_count //= 2
-        total_bastion_count += bastion_count
-        
-        print("The phrase 'Those Were the Days' appears", bastion_count, "times in the file", log_file_path)
-    
-    print("The phrase 'Those Were the Days' appears", total_bastion_count, "times in all log files.")
     
     with open("bastion.txt", "w") as count_file:
         count_file.write(str(total_bastion_count))
-
-    for log_file_path in log_file_paths:
-        fortress_count = 0
-        
-        with open(log_file_path, 'r') as f:
-            for line in f:
-                if "A Terrible Fortress" in line:
-                    fortress_count += 1
-        
-        fortress_count //= 2
-        total_fortress_count += fortress_count
-        
-        print("The phrase 'A Terrible Fortress' appears", fortress_count, "times in the file", log_file_path)
-    
-    print("The phrase 'A Terrible Fortress' appears", total_fortress_count, "times in all log files.")
     
     with open("fortress.txt", "w") as count_file:
         count_file.write(str(total_fortress_count))
